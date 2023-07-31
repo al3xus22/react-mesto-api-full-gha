2a7589066cad17a800fc499661bfa996e3a9a384
Aleksey Oleynik <al3xus22@yandex.ru>
2023-07-31 18:51:38 +0300

    Место - фронтенд + бэкенд

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -92,7 +92,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: 'http://localhost:3001',
+  baseUrl: 'http://localhost:3000',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
